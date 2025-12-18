9819a7cffdd35c416a866cb634a1df0bc25ba5ac
Sahassawat <yokten10@gmail.com>
2025-12-18 10:25:06 +0700

    update safe area

--- app/src/main/java/com/indybrain/indypos_Android/presentation/orderdetail/OrderDetailScreen.kt
+++ app/src/main/java/com/indybrain/indypos_Android/presentation/orderdetail/OrderDetailScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -96,6 +97,7 @@ fun OrderDetailScreen(
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
+                    .statusBarsPadding()
                     .padding(16.dp)
             ) {
                 IconButton(
