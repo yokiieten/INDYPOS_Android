96bc2a5dfe4242ecfd414d6e32a92dabeecae020
Sahassawat <yokten10@gmail.com>
2026-02-02 10:44:48 +0700

    update versioncode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.indybrain.indypos_Android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 22
+        versionCode = 23
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
