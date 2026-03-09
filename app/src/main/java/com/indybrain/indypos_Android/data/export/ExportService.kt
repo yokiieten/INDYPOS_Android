9a3c9e01c3a68bf36b66634284a22c6abe2994c0
Sahassawat <yokten10@gmail.com>
2026-03-09 09:39:54 +0700

    add ,

--- app/src/main/java/com/indybrain/indypos_Android/data/export/ExportService.kt
+++ app/src/main/java/com/indybrain/indypos_Android/data/export/ExportService.kt
@@ -214,7 +214,7 @@ class ExportService @Inject constructor(
                 arrayOf(
                     addon.id,
                     addon.name,
-                    String.format(Locale.US, "%.2f", addon.price),
+                    String.format(Locale.US, "฿%,.2f", addon.price),
                     addon.isActive.toString(),
                     dateFormat.format(addon.createdAt)
                 )
